5a6eb40c357ce5f938b1688467d8986a864517f4
Kevin Rangel <krangel107@gmail.com>
2021-07-16 16:12:33 -0500

    show causas and tasks

--- resources/views/modules/causes/index.blade.php
+++ resources/views/modules/causes/index.blade.php
@@ -28,6 +28,7 @@
             </div>
         </div>
     </section>
+    @include('partials.modalCauses')
 @endsection
 
 @section("scripts")
@@ -76,6 +77,9 @@
                     tr += 		'<a title="Registro de Causas" href="'+ BASE_URL +'/causes/contracts/record-causes/'+data.id+'" class="" style="margin-left:3px;">'
                     tr += 			'<img src="../backend/images/assets/homework.svg" style="width: 25%">'
                     tr += 		'</a>'
+                    tr += 		'<a id="contacts__btnViewCausas" title="Ver Causas" onclick="getCausas('+data.id+')" class="" style="margin-left:3px;">'
+                    tr += 			'<img src="../backend/images/assets/homework.svg" style="width: 25%">'
+                    tr += 		'</a>'
                     tr += 	'</div>'
                     tr += '</td>'
                     $(row).html(tr)
@@ -120,6 +124,104 @@
                     toastr["error"]("", "¡Error en la consulta de datos!")
                 });
 
+            }
+
+            function getCausas (id) {
+                console.log('id', id);
+                fetch(`list/${id}`)
+                    .then(response => response.json())
+                    .then(data => {
+
+                        console.log('data causes', data);
+
+                        let accordionExample = document.querySelector('#accordionExample');
+                        accordionExample.innerHTML = ''
+
+                        data.forEach(element => {
+                            accordionExample.innerHTML += `
+                                <div class="card">
+                                    <div class="card-header d-flex" id="heading${element.id}">
+                                    <div class="card__informatino d-flex flex-wrap">
+                                        <div class="w-50 mb-2">
+                                            <p class="mb-0"><span class="font-weight-bold">RIT: </span> ${element.number_rit}</p>
+                                        </div>
+                                        <div class="w-50">
+                                            <p class="mb-0"><span class="font-weight-bold">Tribunal: </span> ${element.court}</p>
+                                        </div>
+                                        <div class="w-50">
+                                            <p class="mb-0"><span class="font-weight-bold">Materia: </span> ${element.matter}</p>
+                                        </div>
+                                        <div class="w-50">
+                                            <p class="mb-0"><span class="font-weight-bold">Estado: </span> ${element.status === 1 ? 'Abierta' : 'Cerrada'}</p>
+                                        </div>
+                                    </div>
+                                        <h2 class="mb-0">
+                                            <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapse${element.id}" aria-expanded="false" aria-controls="collapse${element.id}" onclick="getTasks(${element.id})"  >
+                                            Ver tareas
+                                            </button>
+                                        </h2>
+                                    </div>
+
+                                    <div id="collapse${element.id}" class="collapse" aria-labelledby="heading${element.id}" data-parent="#accordionExample">
+                                        <div class="card-body">
+                                            <h5 class="mb-1">
+                                                Tareas
+                                            </h5>
+                                            <div class="" id="causes__modalTasks${element.id}">
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            `
+                        });
+
+                    })
+                    $('#modalCauses').modal('show');
+            }
+
+            function getTasks (id) {
+                console.log('task', id);
+                fetch(`list/tasks/${id}`)
+                    .then(response => response.json())
+                    .then(data => {
+
+                        console.log(data);
+                        let accordion__tasks = document.querySelector(`#collapse${id}`);
+                        accordion__tasks.innerHTML = `
+                            <div class="card-body">
+                                <h5 class="mb-1 text-center">
+                                    Tareas
+                                </h5>
+                                <div class="">
+                                    ${generateItem(data)}
+                                </div>
+                            </div>
+                        `
+                    })
+            }
+
+            function generateItem (data) {
+                let foo = ""
+                data.forEach(element => {
+                    foo += `
+                        <div>
+                            <div class="d-flex flex-wrap justify-content-between" >
+                                <small><span class="font-weight-bold">Responsable: </span> ${element.responsible}</small>
+                                <small ><span class="font-weight-bold">Fecha Limite:</span> ${element.deadline}</small >
+                            </div>
+                            <small>
+                                <span class="font-weight-bold">Descripción: </span>${element.description}
+                            </small>
+                            <div class="d-flex flex-column">
+                                <small>Estado: <span class="${element.status === 1 ? 'text-warning' : 'text-success'}">${element.status === 2 ? 'Realizada' : 'Pendiente'}</span></small>
+                                <small><span class="font-weight-bold">Fecha de realización:</span> ${element.date_realization ? element.date_realization : ''}</small>
+                            </div>
+                        </div>
+                                <hr>
+                            `
+                });
+
+                return foo
             }
     </script>
 @endsection
---
+++ resources/views/partials/modalCauses.blade.php
@@ -0,0 +1,15 @@
+<div class="modal fade modal-primary" id="modalCauses" tabindex="-1" aria-labelledby="myModalLabel160" aria-hidden="true" role="dialog">
+    <div class="modal-dialog modal-dialog-centered modal-lg">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="myModalLabel160">Causas</h5>
+                <button type="button" class="btn-close" data-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <div class="accordion" id="accordionExample">
+                
+                </div>
+            </div>
+         </div>
+    </div>
+</div>
