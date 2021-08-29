033246461fc7e9ef2e60f105b676f5f2b3214fd4
Erain Moya <evmoya.89@gmail.com>
2021-08-29 18:41:17 -0400

    Fixed payment quotes

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -22,53 +22,51 @@ class PaymentController extends Controller
 
     public function returnUrl(Request $request)
     {
-        $token = $request->request->get('token_ws');
+        $token = $_GET['token_ws'] ?? $_POST['token_ws'] ?? null;
+        if (!$token) {
+            // Redirect to dashboard
+                Toastr::error("", "¡Pago rechazado por Webpay!");
+                return redirect('dashboard');
+        }
+        else {
+            $resp = (new Transaction)->commit($token);
+            
+            // Register payment and validate quote
+                if ($resp->status === "AUTHORIZED") {
+                    // Extract quote id
+                        $quoteId = explode("-", $resp->sessionId);
 
-        $req = $request->except('_token');
-        $resp = (new Transaction)->commit($token);
+                    // Search quote
+                        $collection = Collection::find($quoteId[0]);
+                        if (!is_null($collection)) {
+                            // Register payment
+                                Payment::create([
+                                    'collection_id' => $quoteId[0],
+                                    'amount' => number_format((int)$resp->amount, 0, '', '.'),
+                                    'authorizationCode' => $resp->authorizationCode,
+                                    'session_id' => $resp->sessionId,
+                                    'buy_order' => $resp->buyOrder,
+                                    'card_number' => $resp->cardNumber,
+                                    'transaction_date' => $resp->transactionDate
+                                ]);
 
-        // Validate payment
-            if ($resp->status === "FAILED") {
-                // Redirect to dashboard
-                    Toastr::error("", "¡Pago rechazado por Webpay!");
-                    return redirect('dashboard');
-            }
-            else {
-                // Register payment and validate quote
-                    if ($resp->status === "AUTHORIZED") {
-                        // Extract quote id
-                            $quoteId = explode("-", $resp->sessionId);
+                            // Update quote status
+                                $collection->update(['status' => "PAGADA"]);
 
-                        // Search quote
-                            $collection = Collection::find($quoteId[0]);
-                            if (!is_null($collection)) {
-                                // Register payment
-                                    Payment::create([
-                                        'collection_id' => $quoteId[0],
-                                        'amount' => number_format((int)$resp->amount, 0, '', '.'),
-                                        'authorizationCode' => $resp->authorizationCode,
-                                        'session_id' => $resp->sessionId,
-                                        'buy_order' => $resp->buyOrder,
-                                        'card_number' => $resp->cardNumber,
-                                        'transaction_date' => $resp->transactionDate
-                                    ]);
-
-                                // Update quote status
-                                    $collection->update(['status' => "PAGADA"]);
-
-                                // Return response
-                                    Toastr::success("", "¡Pago Procesado!");
-                                    return redirect('dashboard');
-                            }
-                            else {
-                                Toastr::error("", "¡Error al procesar el pago por Appboproc!");
+                            // Return response
+                                Toastr::success("", "¡Pago Procesado!");
                                 return redirect('dashboard');
-                            }
-                    }
-                    else {
-                        Toastr::error("", "¡Error al procesar el pago por Appboproc!");
-                        return redirect('dashboard');
-                    }
-            }
+                        }
+                        else {
+                            Toastr::error("", "¡Error al procesar el pago por Appboproc!");
+                            return redirect('dashboard');
+                        }
+                }
+                else {
+                    Toastr::error("", "¡Error al procesar el pago por Appboproc!");
+                    return redirect('dashboard');
+                }
+        }
+
     }
 }
