0c121c8465956ae8bd34b3b6635cf40dab720dd1
Horacio Alanis <alanisdg@gmail.com>
2021-09-29 19:50:27 -0500

    test

--- routes/api.php
+++ routes/api.php
@@ -20,7 +20,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 
-Route::get('/user', function (Request $request) {
+Route::post('/user', function (Request $request) {
     Pending::create([
         'names'=>$request->name,
         'surnames'=>$request->email,
